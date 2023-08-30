f279a927962701bc6432f7a508f4a4bb26a829ae
Reza Soltani <reza.soltani@gmail.com>
2023-08-30 13:48:04 +0330

    add style sheet to project and

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Fast Food Catalogue</h1>
+      <h1>فست فود کاتالوگ</h1>
     </div>
   );
 }
