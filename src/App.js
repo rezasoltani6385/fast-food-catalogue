5fd2524ab36b1dcd9bffbb4c49321bdfd4adc629
Reza Soltani <reza.soltani@gmail.com>
2023-08-30 14:23:00 +0330

    some modifications

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
+import CategoryList from './CategoryList/categoryList';
+import Header from './Header/header';
 
 function App() {
+
+  useEffect(()=>{
+    
+  },[])
+
   return (
-    <div>
-      <h1>فست فود کاتالوگ</h1>
+    <div className='wrapper bg-faded-dark'>
+      <Header></Header>
+      <CategoryList></CategoryList>
     </div>
   );
 }
