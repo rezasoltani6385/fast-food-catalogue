ca6ce4037678c7d1245708d725748215eb976799
Reza Soltani <reza.soltani@gmail.com>
2023-11-01 12:12:24 +0330

    some necessary modifications

--- src/FastFoodItem/fastFoodItem.jsx
+++ src/FastFoodItem/fastFoodItem.jsx
@@ -5,10 +5,12 @@ import {CiCirclePlus} from 'react-icons/ci'
 import {CiCircleMinus} from 'react-icons/ci'
 import { useDispatch } from 'react-redux'
 import { addToCartAction } from '../Redux/action'
+import { useState } from 'react'
 
-const FastFoodItem = ({name, price, ingredients, imageUrl, delay, count}) => {
+const FastFoodItem = ({name, price, ingredients, imageUrl, delay, commentsCount}) => {
     const dispatch = useDispatch()
-
+    const [orderCount, setOrderCount] = useState(1)
+    const [orserSent, setOrderSent] = useState(false)
 
     function scrollToComments() {
         const commentsElement = document.getElementById("comments");
@@ -17,11 +19,14 @@ const FastFoodItem = ({name, price, ingredients, imageUrl, delay, count}) => {
         }
     }
 
+    const addtoCart = (e)=> {
+        e.preventDefault()
+        setOrderSent(true)
+        dispatch(addToCartAction({name: name, price: price, count: orderCount}))
+    }
 
     return (
-        <div className="card product-card h-100 border-0 shadow-sm pb-1 fade-in-horiz"
-            style={{animationDelay: `${delay}s`}}
-        >
+        <div className="card product-card h-100 border-0 shadow-sm pb-1 fade-in-horiz" style={{animationDelay: `${delay}s`}}>
             <span className="badge badge-end badge-shadow bg-success fs-md fw-medium">
                 قیمت : {price.toLocaleString()} تومان
             </span>
@@ -34,32 +39,29 @@ const FastFoodItem = ({name, price, ingredients, imageUrl, delay, count}) => {
                     {ingredients}
                 </div>
                 <div className="d-flex align-items-center justify-content-around mt-auto">
-                    <div className='d-flex align-items-center justify-content-center'>
-                        <button className='btn border-0 text-success p-0 fs-2'
-                            // onClick={() => dispatch(increaseCount({name: key}))} disabled={cartItems[key].count === 10}
-                        >
+                    <div className='d-flex align-items-center justify-content-center my-auto'>
+                        <button className='btn border-0 text-success p-0 ms-2 fs-2' onClick={() => setOrderCount(orderCount + 1)} disabled={orderCount === 10}>
                             <CiCirclePlus/>
                         </button>
                         <p className='p-0 text-center my-auto'>
-                            {/* {cartItems[key].count} */} co
+                            {orderCount}
                         </p>
-                        <button className='btn border-0 p-0 fs-2'
-                            // onClick={() => dispatch(decreaseCount({name: key}))} disabled={cartItems[key].count === 1}
-                        >
+                        <button className='btn border-0 p-0 me-2 fs-2' onClick={() => setOrderCount(orderCount - 1)} disabled={orderCount === 1}>
                             <CiCircleMinus className='text-warning'/>
                         </button>
                     </div>
                     <div className="btn btn-outline-success btn-sm w-60 my-auto fw-bold" role='button'
-                        onClick={()=> dispatch(addToCartAction({name: name, price: price, count: 1}))}
+                        onClick={addtoCart}
                     >
                         <HiShoppingCart className='fs-5 ms-1'/>
                         افزودن به سبد خرید
                     </div>
                     <div className='btn' onClick={scrollToComments} role='button'>
-                        <sup className='text-success'>{count}</sup>
+                        <sup className='text-success'>{commentsCount}</sup>
                         <FaRegComments className='text-success fs-4'/>
                     </div>
                 </div>
+                <div id={`${orserSent ? 'hideMeAfter5Seconds' : ''}`} className={`${orserSent ? 'mt-1' : 'd-none mt-1'}`}>با موفقیت به سبد خرید اضافه شد</div>
             </div>
         </div>
     )
